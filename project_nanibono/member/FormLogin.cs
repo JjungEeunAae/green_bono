fa964503e44cf4853807fd45d670ae0feb88c4a7
Namee572 <myiddab@gmail.com>
2024-02-24 15:39:05 +0900

    오늘은 토요일 로그인 만든거 올린당당

--- project_nanibono/member/FormLogin.cs
+++ project_nanibono/member/FormLogin.cs
@@ -1,4 +1,5 @@
 ﻿using Oracle.ManagedDataAccess.Client;
+using project_nanibono.member;
 
 namespace project_nanibono
 {
@@ -21,7 +22,49 @@ namespace project_nanibono
 
         private void loginButton_Click(object sender, EventArgs e)
         {
+            string memberId = idTextBox.Text.Trim();
+            string password = pwTextBox.Text.Trim();
 
+            if (string.IsNullOrEmpty(memberId))
+            {
+                MessageBox.Show("아이디를 입력하세요.", "경고", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            else if (string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("비밀번호를 입력하세요.", "경고", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            MemberDB memberDB = new MemberDB();
+            Member member = memberDB.SelectMember(memberId, password);
+
+            Member memberr = memberDB.SelectId(memberId);
+            Member memberrr = memberDB.SelectPw(password);
+
+            if (memberr == null)
+            {
+
+                MessageBox.Show("존재하지 않는 회원입니다.", "알림", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            else if (memberrr == null)
+            {
+                MessageBox.Show("비밀번호가 올바르지 않습니다.", "알림", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+
+            // 로그인 성공  // ++++++++로그인유지시켜 
+
+            GlobalVariables.LoggedInUserId = memberId;
+
+            // WordSearch폼은 만들어지는중 
+            // FormWordSearch formWordSearch = new FormWordSearch();
+            //  formWordSearch.Show();
+
+
+            this.Hide();
         }
 
         private void FormLogin_Load(object sender, EventArgs e)
@@ -31,11 +74,6 @@ namespace project_nanibono
 
 
 
-
-        //       private void form3_load(object sender, eventargs e)
-        //      {
-
-        //    }
 
 
         private void SignUpButton_Click(object sender, EventArgs e)
@@ -65,10 +103,13 @@ namespace project_nanibono
 
         }
 
+        private void homeButton_Click(object sender, EventArgs e)
+        {
 
-        //     private void idTextBox_TextChanged(object sender, EventArgs e)
-        //   {
+            // FormWordSearch formWordSearch = new FormWordSearch();
+            //formWordSearch.Show();
 
-        // }
+            // this.Hide();
+        }
     }
 }
--- project_nanibono/member/MemberDB.cs
+++ project_nanibono/member/MemberDB.cs
@@ -1,12 +1,120 @@
-﻿namespace project_nanibono.member
+﻿using Oracle.ManagedDataAccess.Client;
+
+namespace project_nanibono.member
 {
     public class MemberDB
     {
         // 공통 > 로그인 ( 응쟁 )
 
+        string strconn = "Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=192.168.0.110)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=xe)));User Id=bono;Password=bono;";
+
+        public Member SelectMember(string id, string pw)
+        {
+            OracleConnection oc = new OracleConnection(strconn);
+
+            oc.Open();
+
+            string sql = $"select * from Member where id = '{id}' and pw = '{pw}'";
+            OracleCommand cmd = new OracleCommand(sql, oc);
+            OracleDataReader oracleDataReader = cmd.ExecuteReader();
+            if (oracleDataReader.Read())
+            {
 
 
 
+                Member dbMember = new Member()
+                {
+                    id = oracleDataReader["Id"].ToString(),
+                    pw = oracleDataReader["PW"].ToString(),
+
+                };
+
+                return dbMember;
+
+
+            }
+            else
+            {
+
+                return null;
+
+
+            }
+
+            oc.Close();
+        }
+
+        public Member SelectId(string id)
+        {
+            OracleConnection oc = new OracleConnection(strconn);
+
+            oc.Open();
+
+            string sql = $"select * from Member where id = '{id}'";
+            OracleCommand cmd = new OracleCommand(sql, oc);
+            OracleDataReader oracleDataReader = cmd.ExecuteReader();
+            if (oracleDataReader.Read())
+            {
+
+
+
+                Member dbMember = new Member()
+                {
+                    id = oracleDataReader["Id"].ToString(),
+
+
+                };
+
+                return dbMember;
+
+
+            }
+            else
+            {
+
+                return null;
+
+
+            }
+
+            oc.Close();
+        }
+
+        public Member SelectPw(string pw)
+        {
+            OracleConnection oc = new OracleConnection(strconn);
+
+            oc.Open();
+
+            string sql = $"select * from Member where pw = '{pw}'";
+            OracleCommand cmd = new OracleCommand(sql, oc);
+            OracleDataReader oracleDataReader = cmd.ExecuteReader();
+            if (oracleDataReader.Read())
+            {
+
+
+
+                Member dbMember = new Member()
+                {
+
+                    pw = oracleDataReader["PW"].ToString(),
+
+                };
+
+                return dbMember;
+
+
+            }
+            else
+            {
+
+                return null;
+
+
+            }
+
+            oc.Close();
+        }
 
         // 공통 > 회원가입 ( 강녕 )
 
